848a6f6cff60a995f96d73f63c47af0a6b15a69d
aakash <aakashkkg786@gmail.com>
2023-01-19 21:59:30 +0530

    nodejs and express project

---
+++ index.js
@@ -0,0 +1,38 @@
+const express = require("express")
+const app = express();
+
+const ejs =require('ejs');
+
+
+app.set('view engine','ejs');
+// app.set("view engine","ejs")
+
+var a = 'aakash';
+var b =['raju','ravi','amit'];
+
+app.get('/',(req,res)=>{
+    console.log('Home');
+    res.send('welcome to home');
+    // res.render("home",{Name:a})
+}) 
+
+
+app.get('/About',(req,res)=>{
+    console.log('About')
+res.send('welcome to About');
+// res.render('about',{Arr:b});
+}) 
+
+app.get('/contact',(req,res)=>{
+    console.log('contact')
+    res.send('welcome to contact');
+}) 
+
+// app.listen(100,(req,res)=>{
+//     console.log('sever started');
+    
+// })
+
+app.listen(100,(req,res)=>{
+    console.log("server started");
+})
